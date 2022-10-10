95b266a4f55f3b2e24e41d3373dfdb2a0e29d6f5
Mohammad Javad <iman7260@gmail.com>
2022-10-10 21:55:27 +0330

    Change Code and refactoring code

--- bot-services/bot/index.js
+++ bot-services/bot/index.js
@@ -2,8 +2,6 @@ const configurationBot = require("./configurationBot")
 
 module.exports = {
     initialBot: (bot) => {
-        new configurationBot(bot).initReaction();
-        new configurationBot(bot).initText();
         new configurationBot(bot).startBot();
     }
 }
